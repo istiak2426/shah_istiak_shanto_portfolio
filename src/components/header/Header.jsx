9cd8dceb7c51d9cf69fefbf0047edf8c2ba6402c
istiak.shanto <55758239+istiak2426@users.noreply.github.com>
2025-04-26 15:27:24 +0600

    update img attribute with lazy load

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <CTA />
         <HeaderSocials/>
         <div className="me">
-          <img src={ME} alt="me" />
+          <img src={ME} alt="me" loading="lazy" />
         </div>
 
         <a href="#about" className='scroll__down'>Scroll Down</a>
