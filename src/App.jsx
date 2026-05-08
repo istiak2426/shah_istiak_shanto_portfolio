61a241462b9e5f6f761ec472d32c14b4e8d3ee0f
istiak.shanto <55758239+istiak2426@users.noreply.github.com>
2026-05-08 20:42:00 +0600

    Add ChatBot component to App

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Portfolio from "./components/portfolio/Portfolio"
 import Testimonials from "./components/testimonials/Testimonials"
 import Contact from "./components/contact/Contact"
 import Footer from "./components/footer/Footer"
+import ChatBot from "./components/chatbot/ChatBot";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
     <Testimonials />
     <Contact />
     <Footer />
+    <ChatBot />
 
     </>
   )
